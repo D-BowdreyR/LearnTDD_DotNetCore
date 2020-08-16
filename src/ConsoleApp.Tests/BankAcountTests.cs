e7e5b065d4af0ef8d8dbf8ee35f3c14412cf3ba2
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2020-08-16 18:46:51 +0100

    add a few basic tests and go red green refactor

--- src/ConsoleApp.Tests/BankAcountTests.cs
+++ src/ConsoleApp.Tests/BankAcountTests.cs
@@ -10,11 +10,28 @@ namespace ConsoleApp.Tests
         // }
 
         [Test]
-        public void Test1()
+        public void Testdeposit()
         {
             BankAccount acc = new BankAccount();
             acc.deposit(50);
-            Assert.AreEqual(acc.balance, 50);
+            Assert.AreEqual(50, acc.getBalance());
+        }
+
+        [Test]
+        public void Testwithdraw()
+        {
+            BankAccount acc = new BankAccount(75);
+            acc.withdraw(50);
+            Assert.AreEqual(25, acc.getBalance());
+        }
+
+        [Test]
+        public void withdraw_BalanceBecomesNegative_ExpectBalanceToBeReducedByPenalty()
+        {
+            BankAccount acc = new BankAccount(30);
+            acc.withdraw(40);
+            var expected_balance_after_penalty = (30 - 40)-15;
+            Assert.AreEqual(expected_balance_after_penalty, acc.getBalance());
         }
     }
 }
--- src/ConsoleApp/BankAccount.cs
+++ src/ConsoleApp/BankAccount.cs
@@ -4,11 +4,35 @@ namespace ConsoleApp.Tests
 {
     public class BankAccount
     {
-        public double balance;
+        private double balance;
+
+        public BankAccount(int v)
+        {
+            balance = v;
+        }
+
+        public BankAccount()
+        {
+            balance = 0;
+        }
 
         public void deposit(int v)
         {
             balance += v;
+        }
+
+        public void withdraw(int i)
+        {
+            balance -= i;
+            if (balance < 0)
+            {
+                balance -= 15;
+            }
+        }
+
+        public double getBalance()
+        {
+            return balance;
         }
     }
 }
